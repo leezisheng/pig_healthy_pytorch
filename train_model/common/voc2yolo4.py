bb15a44da7a1689e1364194b11bfd588a8d96017
leezisheng <a413liqingshui@gmail.com>
2022-05-19 08:45:07 +0800

    The project tracks the movement of the target object (pigs, experimental mice, etc.), so as to count the movement distance, average movement speed and rest time of the target object (rest:  Refers to the situation where the movement distance is less than 4 pixels), the three feature values are taken as the input of the anomaly detection model to judge the health status of the target object, and the movement track of the target object can be recorded and played back.
    Current Achievements:
    Yolov4 and DEEPSort algorithms were used to read the videos to track the target pigs
    Pending:
    1.UI interface code
    2. Use the camera to track the target pig
    3. Extraction, processing and storage of moving distance, moving average speed and stationary time
    4. Anomaly detection model

---
+++ train_model/common/voc2yolo4.py
@@ -0,0 +1,46 @@
+import os
+import random 
+
+xmlfilepath=r'F:\\pig_healthy\\code\\pig_detect\\yolov4_pytorch\\VOCdevkit\\VOC2007\\Annotations'+'\\'
+saveBasePath=r'F:\\pig_healthy\\code\\pig_detect\\yolov4_pytorch\\VOCdevkit\\VOC2007\\ImageSets\\Main'+'\\'
+
+# trainval_percent用于指定(训练集+验证集)与测试集的比例，默认情况下 (训练集+验证集):测试集 = 9:1
+# train_percent用于指定(训练集+验证集)中训练集与验证集的比例，默认情况下 训练集:验证集 = 9:1
+trainval_percent=0.9
+train_percent=1
+
+temp_xml = os.listdir(xmlfilepath)
+total_xml = []
+for xml in temp_xml:
+    if xml.endswith(".xml"):
+        total_xml.append(xml)
+
+num=len(total_xml)  
+list=range(num)  
+tv=int(num*trainval_percent)  
+tr=int(tv*train_percent)  
+trainval= random.sample(list,tv)  
+train=random.sample(trainval,tr)  
+ 
+print("train and val size",tv)
+print("traub suze",tr)
+ftrainval = open(os.path.join(saveBasePath,'trainval.txt'), 'w')  
+ftest = open(os.path.join(saveBasePath,'test.txt'), 'w')  
+ftrain = open(os.path.join(saveBasePath,'train.txt'), 'w')  
+fval = open(os.path.join(saveBasePath,'val.txt'), 'w')  
+ 
+for i  in list:  
+    name=total_xml[i][:-4]+'\n'  
+    if i in trainval:  
+        ftrainval.write(name)  
+        if i in train:  
+            ftrain.write(name)  
+        else:  
+            fval.write(name)  
+    else:  
+        ftest.write(name)  
+  
+ftrainval.close()  
+ftrain.close()  
+fval.close()  
+ftest .close()
