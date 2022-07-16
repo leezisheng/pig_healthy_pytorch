bd25dfdea76c122ebbaa5da17a9bf9bc3f1f49ca
leezisheng <a413liqingshui@gmail.com>
2022-07-17 01:33:28 +0800

    All basic function points are completed, but the code is not yet layered

--- pig_data_processing/Data_batch_calculate.py
+++ pig_data_processing/Data_batch_calculate.py
@@ -8,10 +8,13 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import random
+import matplotlib
+from sklearn.metrics import confusion_matrix
 
 from Data_save import dataset_save
 from feature_correlation_calculation import correlation_calculation
 from Data_PCA import Data_PCA_Operation
+from novelty_detection import pre_scaler , train_test_split , One_SVM
 
 #  ===========================================功能函数======================================================
 '''
@@ -126,6 +129,12 @@ csv_save_path = 'F:\\pig_healthy\\code\\pig_data_processing\\multi_result'+'\\'+
 correlation_heatmap_save_path = 'F:\\pig_healthy\\code\\pig_data_processing\\multi_result'+'\\'+'correlation_heatmap.jpg'
 # 降维度后二维散点图保存地址
 Data_2D_Scatter_img_save_path = 'F:\\pig_healthy\\code\\pig_data_processing\\multi_result'+'\\'+'Data_2D_Scatter.jpg'
+# 异常检测模型保存位置
+novelty_detection_model_save_path = 'F:\\pig_healthy\\code\\pig_data_processing\\multi_result'+'\\'
+# 异常检测模型图片保存位置
+novelty_detection_model_save_path = 'F:\\pig_healthy\\code\\pig_data_processing\\multi_result'+'\\'+'novelty_detection_result.jpg'
+# 混淆矩阵保存地址
+confusion_matrix_save_path        = 'F:\\pig_healthy\\code\\pig_data_processing\\multi_result'+'\\'+'confusion_matrix.jpg'
 
 #  ===========================================主要流程======================================================
 
@@ -247,7 +256,91 @@ Show_2D_Scatter(
                 save_path      = Data_2D_Scatter_img_save_path
                )
 
+#  ===========================================异常检测======================================================
 
+# 数据预处理
+scaler, Operation_list = pre_scaler(dataset = Operation_list, type_str="std")
+# 数据集拆分
+X_train, X_test = train_test_split(Operation_list)
+# 创建训练模型
+clf = One_SVM(train_dataset = X_train)
+# 训练数据
+clf.model = clf.train_model(save_model_dir = novelty_detection_model_save_path)
+# 模型预测
+train_y_list   = clf.predict_model(predict_x_dataset = X_train)
+predict_y_list = clf.predict_model(predict_x_dataset = X_test)
 
+print("训练集预测数据：")
+print(train_y_list)
+print("测试集预测数据：")
+print(predict_y_list)
+
+n_error_train = train_y_list[train_y_list == -1].size
+n_error_test =  predict_y_list[predict_y_list == -1].size
+
+print(n_error_train,",",n_error_test)
+
+#  ===========================================数据展示======================================================
+xx, yy = np.meshgrid(np.linspace(-5, 5, 500), np.linspace(-5, 5, 500))
+# Generate some abnormal novel observations
+X_outliers = np.random.uniform(low=-5, high=0, size=(25, 2))
+
+y_pred_outliers = clf.predict_model(predict_x_dataset = X_outliers)
+n_error_outliers = y_pred_outliers[y_pred_outliers == 1].size
+
+# plot the line, the points, and the nearest vectors to the plane
+Z = clf.model.decision_function(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.title("Novelty Detection")
+#绘制异常样本的区域
+plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7), cmap=plt.cm.PuBu)
+# 绘制正常样本和异常样本的边界
+a = plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
+#绘制正常样本的区域
+plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='palevioletred')
+s = 40
+b1 = plt.scatter(X_train[:, 0], X_train[:, 1], c='white', s=s, edgecolors='k')
+b2 = plt.scatter(X_test[:, 0], X_test[:, 1], c='blueviolet', s=s,
+                 edgecolors='k')
+c = plt.scatter(X_outliers[:, 0], X_outliers[:, 1], c='gold', s=s,
+                edgecolors='k')
+plt.axis('tight')
+plt.xlim((-5, 5))
+plt.ylim((-5, 5))
+plt.legend([a.collections[0], b1, b2, c],
+           ["learned frontier", "training observations",
+            "new regular observations", "new abnormal observations"],
+           loc="upper left",
+           prop=matplotlib.font_manager.FontProperties(size=11))
+plt.xlabel(
+    "error train: %d/190 ; errors novel regular: %d/19 ; "
+    "errors novel abnormal: %d/50"
+    % (n_error_train, n_error_test, n_error_outliers))
+
+plt.savefig(novelty_detection_model_save_path)
+plt.show()
+#  ===========================================混淆矩阵计算======================================================
+y_pre = []
+y_true  = [1]*len(y_pred_outliers)
+y_pre   = y_pred_outliers
+classes = ['-1', '1']
+
+confusion = confusion_matrix(y_true, y_pre)
+plt.imshow(confusion, cmap=plt.cm.Blues) #此处可以修改为其他颜色
+indices = range(len(confusion))
+plt.xticks(indices, classes)
+plt.yticks(indices, classes)
+plt.colorbar()
+plt.xlabel('y_pred')
+plt.ylabel('y_true')
+
+#显示数据
+for first_index in range(len(confusion)):
+    for second_index in range(len(confusion[first_index])):
+        plt.text(first_index,second_index,confusion[second_index][first_index])
+
+plt.savefig(confusion_matrix_save_path) #保存结果
+plt.show()
 
 
